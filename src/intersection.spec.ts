8c9ce4fab4960f1e45a9172ca579bf1a6752f314
Jack Williams <jw@jackw.io>
2018-07-01 19:41:40 +0100

    finish intersection types

--- src/intersection.spec.ts
+++ src/intersection.spec.ts
@@ -5,14 +5,27 @@ const Type = contract.Type;
 import { expect } from 'chai';
 import * as mocha from 'mocha';
 
+/*
+ * Setup some types
+ */
 const posToBoolean = Type.and(Base.function, Type.fun([Base.positive], Base.boolean));
 const evenToString = Type.and(Base.function, Type.fun([Base.even], Base.string));
 const overload1 = Type.and(Base.function, Type.fun([posToBoolean], Base.number));
 const overload2 = Type.and(Base.function, Type.fun([evenToString], Base.string));
 const functionType = Type.intersection(overload1, overload2);
+const numToNum = Type.and(Base.function, Type.fun([Base.number], Base.number));
+const booleanNegate = Type.and(Base.function, Type.fun([Base.boolean], Base.boolean));
+const plus = Type.and(Base.function, Type.fun([Base.number], numToNum));
+const plusOrNegateType = Type.intersection(plus, booleanNegate);
+const trueThenNumber = Type.and(Base.function, Type.fun([Base.true], Base.number));
+const falseThenString = Type.and(Base.function, Type.fun([Base.false], Base.string));
+const ifThenNumberElseString = Type.intersection(trueThenNumber, falseThenString);
+const evenThenTrue = Type.and(Base.function, Type.fun([Base.even], Base.true));
+const oddThenFalse = Type.and(Base.function, Type.fun([Base.odd], Base.false));
+const stringThenNtoN = Type.and(Base.function, Type.fun([Base.string], numToNum));
 
 describe("Intersection", () => {
-    describe("higher-order overload", () => {
+    describe("with higher-order overload", () => {
         function impl(f: any) {
             const result = f(4);
             if (typeof result === "boolean") return result ? 1 : 0;
@@ -40,14 +53,7 @@ describe("Intersection", () => {
         });
     });
 
-    describe("higher-order return type", () => {
-        const numToNum = Type.and(Base.function, Type.fun([Base.number], Base.number));
-        const booleanNegate = Type.and(Base.function, Type.fun([Base.boolean], Base.boolean));
-
-        const plus = Type.and(Base.function, Type.fun([Base.number], numToNum));
-
-        const plusOrNegateType = Type.intersection(plus, booleanNegate);
-
+    describe("with higher-order return type", () => {
         function plusOrNegate(x: any): any {
             if (typeof x === "number") {
                 return (y: any) => x + y;
@@ -58,11 +64,11 @@ describe("Intersection", () => {
         const plusOrNegateWrapped = contract.assert(
             plusOrNegate, "plus or negate", plusOrNegateType);
 
-        it("should not throw when correct applied twice", () => {
+        it("should not throw when correctly applied twice", () => {
             expect(() => plusOrNegateWrapped(3)(4)).to.not.throw();
         });
 
-        it("should not throw when correct applied once with first-order return", () => {
+        it("should not throw when correctly applied once with first-order return", () => {
             expect(() => plusOrNegateWrapped(true)).to.not.throw();
             expect(() => plusOrNegateWrapped(false)).to.not.throw();
         });
@@ -72,81 +78,65 @@ describe("Intersection", () => {
             expect(() => plusOrNegateWrapped(true)(1)).to.throw();
         });
     });
-});
 
-/** 
-const trueThenNumber = Type.and(Base.function, Type.fun([Base.true], Base.number));
-const falseThenString = Type.and(Base.function, Type.fun([Base.false], Base.string));
-const ifThenNumberElseString = Type.intersection(trueThenNumber, falseThenString);
+    describe("with singleton types for conditionals", () => {
+        function cond(x: any): any {
+            return x ? 10 : "ten";
+        }
+        const wrappedConditional = contract.assert(cond, "conditional", ifThenNumberElseString);
 
-function f(x) {
-    return x ? 10 : "ten";
-}
+        it("should not throw when given true or false", () => {
+            expect(() => wrappedConditional(true)).to.not.throw();
+            expect(() => wrappedConditional(false)).to.not.throw();
+        });
 
-f = contract.assert(f, "conditional", ifThenNumberElseString);
+        it("should throw when given non-boolean", () => {
+            expect(() => wrappedConditional(0)).to.throw();
+            expect(() => wrappedConditional("true")).to.throw();
+        });
+    });
 
-console.log("f(true): " + f(true));
-console.log("f(false): " + f(false))
-
-try {
-    // Not true or false, so negative blame!
-    console.log("f(0): " + f(0));
-} catch (e) {
-    console.log(e);
-}
-
-const evenThenTrue = Type.and(Base.function, Type.fun([Base.even], Base.true));
-const oddThenFalse = Type.and(Base.function, Type.fun([Base.odd], Base.false));
-const stringThenNtoN = Type.and(Base.function, Type.fun([Base.string], numToNum));
-
-const mega =
-    Type.intersection(
-        trueThenNumber,               // true -> Number
-        Type.intersection(
-            falseThenString,          // false -> String
+    describe("with nesting", () => {
+        const mega =
             Type.intersection(
-                evenThenTrue,         // Even -> true
+                trueThenNumber,
                 Type.intersection(
-                    oddThenFalse,     // Odd -> false
-                    stringThenNtoN    // String -> Number -> Number
+                    falseThenString,
+                    Type.intersection(
+                        evenThenTrue,
+                        Type.intersection(
+                            oddThenFalse,
+                            stringThenNtoN
+                        )
+                    )
                 )
-            )
-        )
-    );
+            );
 
-console.log(mega);
+        function bigSwitch(x: any): any {
+            switch (typeof x) {
+                case "boolean": return x ? 1 : "hello world";
+                case "number": return x % 2 === 0;
+                case "string": return (y: any) => y + x.length;
+            }
+        }
 
-function bigSwitch(x) {
-    switch (typeof x) {
-        case "boolean": return x ? 1 : "hello world";
-        case "number": return x % 2 === 0;
-        case "string": return y => y + x.length;
-    }
-}
+        const bigSwitchWrapped = contract.assert(bigSwitch, "nested intersection", mega);
 
-bigSwitch = contract.assert(bigSwitch, "nested intersection", mega);
+        it("should not throw when given correct input", () => {
+            expect(() => bigSwitchWrapped(true)).to.not.throw();
+            expect(() => bigSwitchWrapped(false)).to.not.throw();
+            expect(() => bigSwitchWrapped(0)).to.not.throw();
+            expect(() => bigSwitchWrapped(1)).to.not.throw();
+            expect(() => bigSwitchWrapped("a long string")(42)).to.not.throw();
 
-console.log("bigSwitch(true): " + bigSwitch(true));
-console.log("bigSwitch(false): " + bigSwitch(false));
-console.log("bigSwitch(0): " + bigSwitch(0));
-console.log("bigSwitch(1): " + bigSwitch(1));
-console.log("bigSwitch(\"a long string\")(29): " + bigSwitch("a long string")(29));
+        });
 
-try {
-    // Passing an undefined that does not satisfy any of the domain
-    // types, yielding negative blame.
-    bigSwitch(undefined);
-} catch (e) {
-    console.log(e);
-}
+        it("should throw when given undefined", () => {
+            expect(() => bigSwitchWrapped(undefined)).to.throw();
+        });
 
-try {
-    // Passing a string selects the overload String -> Number ->
-    // Number, however we supply a string as the second argument so
-    // negative blame is raised.
-    bigSwitch("a string")("another string");
-} catch (e) {
-    console.log(e);
-}
-
-*/
+        it("should throw when supplying non-number to string overload return function", () => {
+            expect(() => bigSwitchWrapped("a string")("another string")).to.throw();
+        });
+    });
+});
